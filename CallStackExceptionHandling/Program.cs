63d12578ea33b7460cc5504eab10296ed60e5031
Devan Fields <24418547+Devan1@users.noreply.github.com>
2022-04-20 18:46:54 -0500

    Initial commit

---
+++ CallStackExceptionHandling/Program.cs
@@ -0,0 +1,24 @@
+﻿//using CalculatorLib;
+using CallStackExceptionHandlingLib;
+
+Console.WriteLine("In main");
+Alpha();
+
+static void Alpha()
+{
+    Console.WriteLine("In Alpha");
+    Beta();
+}
+static void Beta()
+{
+    Console.WriteLine("In Beta");
+    //Calculator.Gamma();
+    try
+    {
+        Calculator.Gamma();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Caught this exception: {ex.Message}");
+    }
+}
---
+++ Debugging/Program.cs
@@ -0,0 +1,12 @@
+﻿double a = 4.5;
+double b = 2.5;
+double answer = Add(a, b);
+Console.WriteLine($"{a} + {b} = {answer}");
+
+Console.WriteLine("Press any key to exit. ");
+Console.ReadLine();
+
+static double Add(double a, double b)
+{
+    return a * b;//deliberate bug. 
+}
